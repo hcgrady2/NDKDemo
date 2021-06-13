f18b609a09599923526169300688a5723aa4e4db
wanghaichao <hcwang1024@163.com>
2021-06-13 11:35:31 +0800

    ndk study

--- app/src/main/java/com/example/ndkdemo/MainActivity.java
+++ app/src/main/java/com/example/ndkdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ndkdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.ndkdemo.databinding.ActivityMainBinding;
@@ -10,6 +11,8 @@ import com.example.ndkdemo.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
+
+    private static final String TAG = "MainActivityTag";
     static {
         System.loadLibrary("native-lib");
     }
@@ -26,6 +29,44 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+
+        String result = operateString("待操作的字符串");
+        binding.operateStr.setText(result);
+
+
+        try {
+
+            int array[] = new int[]{1,2,3,4,5};
+            int sum  = sumArray(array);
+
+            Log.i(TAG, "onCreate: sum :" + sum);
+
+            binding.tvSum.setText(String.valueOf(sum));
+        }catch (Exception e){
+
+            Log.i(TAG, "onCreate: exception:" + e.toString());
+        }
+
+
+
+        //交给native层创建->Java打印输出
+        int[][] init2DArray = init2DArray(3);
+        for (int i = 0; i < 3; i++) {
+            for (int i1 = 0; i1 < 3; i1++) {
+                Log.i(TAG, "init2DArray[" + i + "][" + i1 + "]" + " = " + init2DArray[i][i1]);
+            }
+        }
+
+
+        String tvStatic = callStaticMethod("这里是入参数");
+        binding.tvStaticMethod.setText(tvStatic);
+
+
+        callAndCreateIntanceMethod();
+
+        testNativeCrash();
+
     }
 
     /**
@@ -33,4 +74,29 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public static native void ginsengTest(short s, int i, long l, float f, double d, char c,
+                                          boolean z, byte b, String str, Object obj, MainActivity p, int[] arr);
+
+    //c++ 操作字符串
+    public native String operateString(String str);
+
+
+    //c++ 数组求和
+    public native int sumArray(int[] array);
+
+    //native 创建数组并返回给 java
+    public native int[][] init2DArray(int size);
+
+
+    //c++ 调用静态方法，有入参有返回值
+    public native String callStaticMethod(String str);
+
+
+    //c++ 调用实例方法
+    public native void callAndCreateIntanceMethod();
+
+    //测试 c++ 崩溃
+    public native void testNativeCrash();
+
 }
